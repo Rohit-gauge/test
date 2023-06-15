28608834be364a30ac8a1c76e1a961b3a9ec8b3b
Rohit Bharti <71224966+Smarty6452@users.noreply.github.com>
2023-06-15 14:19:51 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Header from './components/planning/index'
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Header/nav";
+import Home from "./components/Pages/home";
+import Bottles from "./components/Pages/bottle";
+import About from "./components/Pages/about";
+import Product from "./components/Pages/products";
+import Contact from "./components/Pages/contacts";
 
 const App = () => {
   return (
-    <>
-    <Navbar />
-    <Header />
-    </>
-  )
-}
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route  path="/" element={<Home />} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/bottles" element={<Bottles />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className="container">
+      <div>
+        home
+      </div>
+    </div>
+  )
+}
+
+export default home
